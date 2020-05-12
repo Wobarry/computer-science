eebf8acb5f7cd9a8784c405c8c33f72a4e7e5499
henry-glitch <hwatson967@bristolcathedral.org.uk>
2020-05-12 21:06:08 +0100

    python code and demo

---
+++ demo.py
@@ -0,0 +1,46 @@
+#comments use # 
+
+
+# print function
+print('hello world')
+#string concatenation
+print('hello '+ 'world')
+#variables store data eg :
+num = 2
+#arithmatic
+#times:
+print(1 * 2) 
+#divide:
+print(1/2)
+#subtract:
+print(1-2)
+#addition
+print(1+2)
+#remainder
+print(1%2)
+#+= operater
+num += 2
+#-= operator
+num-=2
+#integers or int are just normal numbers eg
+int0 = 7
+# there are also floats that store deccimal point numbers
+float0 = 7.2
+#strings are a set of charecters eg:
+string0 ='Hello world'
+#booleans 
+bool0 = True
+#number and string concatination
+print('Hello my favourit number is ' + str(int0))
+#this causes an problem ending the string
+print('There's a problem here ')
+#to fix this we use the backslah charecter \ :
+print('There\'s not problem here ')
+#numbers in python start with 0 instead of one eg:
+#+---+---+---+---+---+---+
+#| P | Y | T | H | O | N |
+#+---+---+---+---+---+---+
+#  0   1   2   3   4   5
+print(string0.1)
+#this would print the e of Hello world
+
